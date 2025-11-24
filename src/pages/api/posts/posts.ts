7433b4bbca102a5f2f6d4f14e4ae234f6e1e7b45
Mauricio-jara <maur.jaraa@duocuc.cl>
2025-11-24 11:12:16 -0300

    deleting error

--- src/pages/api/posts/posts.ts
+++ src/pages/api/posts/posts.ts
@@ -18,15 +18,3 @@ export async function GET() {
 
   return new Response(JSON.stringify(posts), { status: 200 });
 }
-
-Esto devuelve un JSON así:
-
-{
-  "id": 1,
-  "title": "Hola mundo",
-  "content": "Texto",
-  "author": { "id": 3, "username": "Mauricio" },
-  "comments": [
-    { "id": 10, "content": "hola", "author": { "id": 4, "username": "Pedro" } }
-  ]
-}
